9f0dd7ca37d6e6a6dab493ea65b95625da043056
arshavskiy <pasha.websbee@gmail.com>
2017-10-23 23:00:14 +0300

    style added

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -8,12 +8,19 @@ import { Component, Input } from '@angular/core';
     <Br>
     <h4>Date: {{event.date}}</h4>
     <h4>Price: \${{ event.price }}</h4>
-    <span> location {{event.location.address}}, {{event.location.city}}, {{event.location.country}}</span>
+    <span><h4 class="subheader"> Location: </h4></span><span> {{event.location.address}}, {{event.location.city}}, {{event.location.country}}</span>
     <h4>
         <img src='{{event.imageUrl}}'>
     </h4>
 </div>
-    `
+    `,
+    styles: [`
+    .subheader{
+        font-size:20px;
+        color:white;
+        display: inline;
+    }
+    `]
 })
 export class EventThumbnailComponent {
     @Input() event: any //event object
